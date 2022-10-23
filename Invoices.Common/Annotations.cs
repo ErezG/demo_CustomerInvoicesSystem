1ee83a69069e125f974d51f986c070c40488752a
Erez laptop <erezgiv.work@gmail.com>
2022-10-23 16:48:19 +0300

    add input validation via annotations

---
+++ Invoices.Common/Annotations.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Invoices.Common
+{
+    public class RequiredEnumFieldAttribute : RequiredAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            var type = value.GetType();
+            return type.IsEnum && Enum.IsDefined(type, value);
+        }
+    }
+    public class RequiredDoubleFieldAttribute : RequiredAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            var type = value.GetType();
+            return type == typeof(double) && (double)value != 0;
+        }
+    }
+}
--- Invoices.Common/InvoiceCreationModel.cs
+++ Invoices.Common/InvoiceCreationModel.cs
@@ -1,10 +1,17 @@
-﻿namespace Invoices.Common
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Invoices.Common
 {
     public class InvoiceCreationModel
     {
         public DateTime CreatedOn = DateTime.UtcNow;
         public ProcessingStatuses Status = ProcessingStatuses.New;
+
+        [RequiredDoubleFieldAttribute(ErrorMessage = "Amount Must not be zero.")]
         public double Amount { get; set; }
+
+        [RequiredEnumFieldAttribute(ErrorMessage = "Method value is invalid.")]
         public PaymentMethods Method { get; set; }
     }
 }
